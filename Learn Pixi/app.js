b28b9a3af76f86a903a02500ded96a8c07777c5f
HermantNET <hermant.edu@gmail.com>
2016-11-22 13:50:12 -0500

    Added movement, rectangle and state

--- Learn Pixi/app.js
+++ Learn Pixi/app.js
@@ -1,3 +1,40 @@
+function keyboard(keyCode) {
+  var key = {};
+  key.code = keyCode;
+  key.isDown = false;
+  key.isUp = true;
+  key.press = undefined;
+  key.release = undefined;
+  //The `downHandler`
+  key.downHandler = function(event) {
+    if (event.keyCode === key.code) {
+      if (key.isUp && key.press) key.press();
+      key.isDown = true;
+      key.isUp = false;
+    }
+    event.preventDefault();
+  };
+
+  //The `upHandler`
+  key.upHandler = function(event) {
+    if (event.keyCode === key.code) {
+      if (key.isDown && key.release) key.release();
+      key.isDown = false;
+      key.isUp = true;
+    }
+    event.preventDefault();
+  };
+
+  //Attach event listeners
+  window.addEventListener(
+    "keydown", key.downHandler.bind(key), false
+  );
+  window.addEventListener(
+    "keyup", key.upHandler.bind(key), false
+  );
+  return key;
+}
+
 // Aliases
 var Container = PIXI.Container,
 	autoDetectRenderer = PIXI.autoDetectRenderer,
@@ -5,20 +42,20 @@ var Container = PIXI.Container,
 	resources = PIXI.loader.resources,
 	Sprite = PIXI.Sprite,
 	TextureCache = PIXI.utils.TextureCache,
-	Rectangle = PIXI.Rectangle;
+	Rectangle = PIXI.Rectangle,
+	Graphics = PIXI.Graphics;
 
 	
 // Create stage, renderer.
 // Add renderer.view to DOM
 var stage = new Container(),
-	renderer = autoDetectRenderer(256, 256, {antialias: true});
+	renderer = autoDetectRenderer(256, 256, {antialias: true, transparent: true});
 document.body.appendChild(renderer.view);
-
+renderer.view.setAttribute("tabIndex", 1);
 
 // Load assets, run 'setup' when all loaded
 loader
 	.add("images/cat.png")
-	.add("images/tileset.png")
 	.on("progress", loadProgressHandler)
 	.load(setup);
 
@@ -27,55 +64,112 @@ function loadProgressHandler(loader) {
 }
 
 // Sprites
-var rocket;
-var cat;
+var cat, rectangle, state;
 
 function setup() {
-	// create 'tileset' sprite from texture
-	var tileset = TextureCache["images/tileset.png"];
-	
-	// create rect object that defines position & size of
-	// sub-image from 'tileset'
-	var rect = new Rectangle(96, 64, 32, 32);
-	
-	// have 'tileset' use 'rect' lens
-	tileset.frame = rect;
-	
 	// Create sprite from texture
-	rocket = new Sprite(tileset);
 	cat = new Sprite(resources["images/cat.png"].texture);
-	
-	// Position 'rocket'
-	rocket.position.set(32, 32);
 	
 	// Position 'cat'
 	cat.pivot.set(32, 32)
 	cat.anchor.set(0, 1);
 	cat.x = (renderer.view.width / 2);
 	cat.y = (renderer.view.height / 2);
+	cat.vx = 0;
+	cat.vy = 0;
+	
+	// Capture keyboard controls
+	var left = keyboard(37),
+		up = keyboard(38),
+		right = keyboard(39),
+		down = keyboard(40);
+		
+	left.press = function() {
+		cat.vx = -1;
+		cat.vy = 0;
+	}
+	left.release = function() {
+		if (!right.isDown && cat.vy === 0) {
+			cat.vx = 0;
+		}
+	}
+	
+	up.press = function() {
+		cat.vy = -1;
+		cat.vx = 0;
+	};
+	up.release = function() {
+		if (!down.isDown && cat.vx === 0) {
+			cat.vy = 0;
+		}
+	};
+	
+	right.press = function() {
+		cat.vx = +1;
+		cat.vy = 0;
+	}
+	right.release = function() {
+		if (!left.isDown && cat.vy === 0) {
+			cat.vx = 0;
+		}
+	}
+	
+	down.press = function() {
+		cat.vy = 1;
+		cat.vx = 0;
+	};
+	
+	down.release = function() {
+		if (!up.isDown && cat.vx === 0) {
+			cat.vy = 0;
+		}
+	};
 	
 	// Add sprites to the stage
-	stage.addChild(rocket);
 	stage.addChild(cat);
 	
-
+	rectangle = new Graphics();
+	rectangle.lineStyle(4, 0x3F1350, 1);
+	rectangle.beginFill(0x16DC0C);
+	rectangle.drawRect(0, 0, 64, 64);
+	rectangle.endFill();
+	rectangle.x = 170;
+	rectangle.y = 170;
+	rectangle.pivot.set(32, 32);
+	rectangle.alpha = 0.2;
+	stage.addChild(rectangle);
+	
+	// Set the game state
+	state = play;
+	
+	// Start the game loop
 	gameLoop();
 }
 
-var shrink = true;
 function gameLoop() {
 	requestAnimationFrame(gameLoop);
 	
-	cat.rotation += 0.01;
-	if (shrink) {
-		cat.scale.x -= 0.001;
-		cat.scale.y -= 0.001;
-		shrink = !(cat.scale.x <= 0.1);
-	} else {
-		cat.scale.x += 0.001;
-		cat.scale.y += 0.001;
-		shrink = cat.scale.x >= 1;
-	}
+	state();
 	
 	renderer.render(stage);
+}
+
+var playState = {
+	shrink: false
+};
+function play() {
+	cat.x += cat.vx;
+	cat.y += cat.vy;
+	
+	rectangle.rotation += 0.05;
+	
+	if (playState.shrink === false) {
+		rectangle.scale.x += 0.02;
+		rectangle.scale.y += 0.02;
+		if (rectangle.scale.x > 1) playState.shrink = true;
+	} else {
+		rectangle.scale.x -= 0.02;
+		rectangle.scale.y -= 0.02;
+		if (rectangle.scale.x < 0.1) playState.shrink = false;
+	}
 }
